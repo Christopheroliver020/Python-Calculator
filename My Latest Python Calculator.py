188da39b2651b48df89528de5ef362b9afe56e01
Christopher L. Oliver <154577365+Christopheroliver020@users.noreply.github.com>
2023-12-22 14:06:18 +0800

    Add files via upload

---
+++ My Latest Python Calculator.py
@@ -0,0 +1,20 @@
+print("""
+_________________________________________
+|                                        |
+|      Welcome to Python Calculator      |
+|                                        |
+|    created by Christopher L. Oliver    |
+|________________________________________|
+""")
+
+firstNumber = int(input("Enter a firstNumber: "))
+secondNumber = int(input("Enter a secondNumber: "))
+
+print("""
+------------------------------------------------------------------------------------------------------------------------------------------------
+""")
+
+print("[ Addition ] output =",  firstNumber + secondNumber)
+print("[ Multiplacation ] output =",  firstNumber * secondNumber)
+print("[ Division ] output =",  firstNumber / secondNumber)
+print("[ Subtraction ] output =",  firstNumber - secondNumber) 
